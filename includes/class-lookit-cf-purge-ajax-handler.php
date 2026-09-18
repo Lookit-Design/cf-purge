a715cd929998a042dc76b90821989a2ca134fef9
Vadim Kononov <vadim@konoson.com>
2026-09-17 22:57:09 -0500

    Handle same-site URLs and fix compatibility checks (#29)
    Compare hostnames case-insensitively so valid same-site URLs are not
    rejected before a Cloudflare purge. Also repair the documented PHP
    compatibility command.\n\nTests cover mixed-case hosts, foreign-host
    rejection, authorization, nonces, the full PHPUnit suite, coding
    standards, and compatibility.

--- includes/class-lookit-cf-purge-ajax-handler.php
+++ includes/class-lookit-cf-purge-ajax-handler.php
@@ -29,7 +29,7 @@ class Lookit_CF_Purge_Ajax_Handler {
 		$site_host = wp_parse_url( home_url(), PHP_URL_HOST );
 		$req_host  = wp_parse_url( $url, PHP_URL_HOST );
 
-		if ( $site_host !== $req_host ) {
+		if ( 0 !== strcasecmp( (string) $site_host, (string) $req_host ) ) {
 			wp_send_json_error( 'URL does not belong to this site.' );
 		}
 
--- tests/test-ajax-handler.php
+++ tests/test-ajax-handler.php
@@ -64,6 +64,17 @@ class Test_Lookit_CF_Purge_Ajax_Handler extends WP_Ajax_UnitTestCase {
 		$this->assertTrue( $response['success'] );
 	}
 
+	public function test_purge_url_accepts_same_site_host_with_different_case() {
+		$this->_setRole( 'administrator' );
+		$this->set_nonce();
+		$host         = wp_parse_url( home_url(), PHP_URL_HOST );
+		$_POST['url'] = str_replace( $host, strtoupper( $host ), home_url( '/sample-page/' ) );
+
+		$response = $this->dispatch( 'lookit_cf_purge_url' );
+
+		$this->assertTrue( $response['success'] );
+	}
+
 	public function test_purge_url_rejects_invalid_url() {
 		$this->_setRole( 'administrator' );
 		$this->set_nonce();
